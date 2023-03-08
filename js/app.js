9b23af3c810ee46f6779975f5b009f6f87b359fd
Diego Veliz <velizdiego82@gmail.com>
2023-03-08 20:09:45 -0300

    cv

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu() {
+    menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
+
+const menulinks = document.querySelectorAll('.menu a[href^="#"]');
+
+menulinks.forEach(menuLinks => {
+    menuLinks.addEventListener ("click", function() {
+        menu.classList.remove("menu_opened");
+    })
+})
